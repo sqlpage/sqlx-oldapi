8bb19411ec6b42bcebc80a5af4fe5c83146c739b
lovasoa <contact@ophir.dev>
2024-11-07 21:27:40 +0100

    faster mssql startup

--- tests/mssql/configure-db.sh
+++ tests/mssql/configure-db.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-# Wait 60 seconds for SQL Server to start up
-sleep 60
+# Wait for SQL Server to be ready for connections
+until /opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -Q "SELECT 1;" -No
+do
+  echo "Waiting for SQL Server to be ready..."
+  sleep 1
+done
 
 # Run the setup script to create the DB and the schema in the DB
 /opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -i setup.sql -No
