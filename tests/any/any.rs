dc88ab9ae9d2e9e94dddecc174ee027a15bf36f2
lovasoa <contact@ophir.dev>
2025-09-07 22:45:10 +0200

    Fix UUID test for MySQL
    The previous test used the wrong SQL for MySQL. This commit corrects it.

--- tests/any/any.rs
+++ tests/any/any.rs
@@ -117,11 +117,13 @@ async fn it_has_json() -> anyhow::Result<()> {
 async fn it_has_uuid() -> anyhow::Result<()> {
     use sqlx_oldapi::types::Uuid;
     #[cfg(feature = "sqlite")]
-    let sql = "CAST('123e4567-e89b-12d3-a456-426614174000' AS TEXT)";
+    let sql = "x'123e4567e89b12d3a456426614174000'";
     #[cfg(feature = "mssql")]
     let sql = "CONVERT(uniqueidentifier, '123e4567-e89b-12d3-a456-426614174000')";
     #[cfg(feature = "postgres")]
     let sql = "CAST('123e4567-e89b-12d3-a456-426614174000' AS UUID)";
+    #[cfg(feature = "mysql")]
+    let sql = "UUID_TO_BIN('123e4567-e89b-12d3-a456-426614174000')";
     assert_eq!(
         Uuid::parse_str("123e4567-e89b-12d3-a456-426614174000")?,
         get_val::<Uuid>(sql).await?
