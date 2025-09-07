fba6797e89398c634b2af475a363808335160248
Ophir LOJKINE <pere.jobs@gmail.com>
2025-09-07 23:23:01 +0200

    add support for the uuid type on the Any database (#30)
    * add support for the uuid type on the Any database
    
    * Fix UUID test for MySQL
    
    The previous test used the wrong SQL for MySQL. This commit corrects it.
    
    * Fix UUID test for various databases
    
    * Fix MSSQL UUID test
    
    Fix MSSQL UUID test
    
    The MSSQL UUID test was failing due to an incorrect query string. This
    commit removes the extra character.

--- tests/any/any.rs
+++ tests/any/any.rs
@@ -112,6 +112,24 @@ async fn it_has_json() -> anyhow::Result<()> {
     Ok(())
 }
 
+#[cfg(feature = "uuid")]
+#[sqlx_macros::test]
+async fn it_has_uuid() -> anyhow::Result<()> {
+    use sqlx_oldapi::types::Uuid;
+    assert_eq!(
+        Uuid::parse_str("123e4567-e89b-12d3-a456-426614174000")?,
+        get_val::<Uuid>(if cfg!(feature = "mssql") {
+            "CONVERT(uniqueidentifier, '123e4567-e89b-12d3-a456-426614174000')"
+        } else if cfg!(feature = "postgres") {
+            "'123e4567-e89b-12d3-a456-426614174000'::uuid"
+        } else {
+            "x'123e4567e89b12d3a456426614174000'"
+        })
+        .await?
+    );
+    Ok(())
+}
+
 #[sqlx_macros::test]
 async fn it_pings() -> anyhow::Result<()> {
     let mut conn = new::<Any>().await?;
