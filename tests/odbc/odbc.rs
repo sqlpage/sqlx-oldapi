54046af628db4f0db8cc96029c900723accfacc4
lovasoa <contact@ophir.dev>
2025-09-19 23:05:17 +0200

    feat: Add ODBC test configuration and workflow
    This commit introduces a new ODBC test configuration in Cargo.toml and adds a corresponding GitHub Actions workflow for running ODBC tests with SQLite. The workflow includes steps for installing necessary dependencies and executing tests.

---
+++ tests/odbc/odbc.rs
@@ -0,0 +1,21 @@
+use sqlx_oldapi::odbc::Odbc;
+use sqlx_oldapi::Connection;
+use sqlx_test::new;
+
+#[sqlx_macros::test]
+async fn it_connects_and_pings() -> anyhow::Result<()> {
+    let mut conn = new::<Odbc>().await?;
+    conn.ping().await?;
+    conn.close().await?;
+    Ok(())
+}
+
+#[sqlx_macros::test]
+async fn it_can_work_with_transactions() -> anyhow::Result<()> {
+    let mut conn = new::<Odbc>().await?;
+    let tx = conn.begin().await?;
+    tx.rollback().await?;
+    Ok(())
+}
+
+
