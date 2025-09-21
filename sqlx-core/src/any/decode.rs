eae840bfbaf7513c4d2e6a8083b4e9f7ccb4a699
lovasoa <contact@ophir.dev>
2025-09-21 02:12:42 +0200

    refactor: Simplify trait generation for database encoding, decoding, and indexing
    This commit refactors the encoding, decoding, and indexing traits in the sqlx-core library by introducing macros to generate the `AnyEncode`, `AnyDecode`, and `AnyColumnIndex` traits based on enabled features. This change enhances maintainability and reduces code duplication, while ensuring comprehensive support for various database combinations, including ODBC.

--- sqlx-core/src/any/decode.rs
+++ sqlx-core/src/any/decode.rs
@@ -58,323 +58,99 @@ macro_rules! impl_any_decode {
     };
 }
 
-// Macro to generate AnyDecode trait and implementation for a given set of databases
-macro_rules! impl_any_decode_for_db {
+// This macro generates the trait and impl based on which features are enabled
+macro_rules! define_any_decode {
     (
-        $(#[$meta:meta])*
-        $($db:ident),+
+        // List all possible feature combinations with their corresponding database lists
+        $(
+            #[cfg($($cfg:tt)*)]
+            [$($db:ident),* $(,)?]
+        ),* $(,)?
     ) => {
-        $(#[$meta])*
-        pub trait AnyDecode<'r>: $(Decode<'r, $db> + Type<$db> + )+ {}
+        $(
+            #[cfg($($cfg)*)]
+            pub trait AnyDecode<'r>: $(Decode<'r, $db> + Type<$db> +)* 'r {}
 
-        $(#[$meta])*
-        impl<'r, T> AnyDecode<'r> for T
-        where
-            T: $(Decode<'r, $db> + Type<$db> + )+
-        {}
+            #[cfg($($cfg)*)]
+            impl<'r, T> AnyDecode<'r> for T
+            where
+                T: $(Decode<'r, $db> + Type<$db> +)* 'r
+            {}
+        )*
     };
 }
 
-// Generate all combinations of databases
-// The order is: Postgres, MySql, Mssql, Sqlite, Odbc
+// Define all combinations in a more compact, maintainable format
+define_any_decode! {
+    // 5 databases
+    #[cfg(all(feature = "postgres", feature = "mysql", feature = "mssql", feature = "sqlite", feature = "odbc"))]
+    [Postgres, MySql, Mssql, Sqlite, Odbc],
 
-// All 5 databases
-impl_any_decode_for_db! {
-    #[cfg(all(
-        feature = "postgres",
-        feature = "mysql",
-        feature = "mssql",
-        feature = "sqlite",
-        feature = "odbc"
-    ))]
-    Postgres, MySql, Mssql, Sqlite, Odbc
-}
+    // 4 databases (5 combinations) - missing one each
+    #[cfg(all(not(feature = "postgres"), feature = "mysql", feature = "mssql", feature = "sqlite", feature = "odbc"))]
+    [MySql, Mssql, Sqlite, Odbc],
+    #[cfg(all(feature = "postgres", not(feature = "mysql"), feature = "mssql", feature = "sqlite", feature = "odbc"))]
+    [Postgres, Mssql, Sqlite, Odbc],
+    #[cfg(all(feature = "postgres", feature = "mysql", not(feature = "mssql"), feature = "sqlite", feature = "odbc"))]
+    [Postgres, MySql, Sqlite, Odbc],
+    #[cfg(all(feature = "postgres", feature = "mysql", feature = "mssql", not(feature = "sqlite"), feature = "odbc"))]
+    [Postgres, MySql, Mssql, Odbc],
+    #[cfg(all(feature = "postgres", feature = "mysql", feature = "mssql", feature = "sqlite", not(feature = "odbc")))]
+    [Postgres, MySql, Mssql, Sqlite],
 
-// 4 databases (5 combinations)
-impl_any_decode_for_db! {
-    #[cfg(all(
-        feature = "mysql",
-        feature = "mssql",
-        feature = "sqlite",
-        feature = "odbc",
-        not(feature = "postgres")
-    ))]
-    MySql, Mssql, Sqlite, Odbc
-}
+    // 3 databases (10 combinations)
+    #[cfg(all(not(any(feature = "postgres", feature = "mysql")), feature = "mssql", feature = "sqlite", feature = "odbc"))]
+    [Mssql, Sqlite, Odbc],
+    #[cfg(all(not(any(feature = "postgres", feature = "mssql")), feature = "mysql", feature = "sqlite", feature = "odbc"))]
+    [MySql, Sqlite, Odbc],
+    #[cfg(all(not(any(feature = "postgres", feature = "sqlite")), feature = "mysql", feature = "mssql", feature = "odbc"))]
+    [MySql, Mssql, Odbc],
+    #[cfg(all(not(any(feature = "postgres", feature = "odbc")), feature = "mysql", feature = "mssql", feature = "sqlite"))]
+    [MySql, Mssql, Sqlite],
+    #[cfg(all(not(any(feature = "mysql", feature = "mssql")), feature = "postgres", feature = "sqlite", feature = "odbc"))]
+    [Postgres, Sqlite, Odbc],
+    #[cfg(all(not(any(feature = "mysql", feature = "sqlite")), feature = "postgres", feature = "mssql", feature = "odbc"))]
+    [Postgres, Mssql, Odbc],
+    #[cfg(all(not(any(feature = "mysql", feature = "odbc")), feature = "postgres", feature = "mssql", feature = "sqlite"))]
+    [Postgres, Mssql, Sqlite],
+    #[cfg(all(not(any(feature = "mssql", feature = "sqlite")), feature = "postgres", feature = "mysql", feature = "odbc"))]
+    [Postgres, MySql, Odbc],
+    #[cfg(all(not(any(feature = "mssql", feature = "odbc")), feature = "postgres", feature = "mysql", feature = "sqlite"))]
+    [Postgres, MySql, Sqlite],
+    #[cfg(all(not(any(feature = "sqlite", feature = "odbc")), feature = "postgres", feature = "mysql", feature = "mssql"))]
+    [Postgres, MySql, Mssql],
 
-impl_any_decode_for_db! {
-    #[cfg(all(
-        feature = "postgres",
-        feature = "mssql",
-        feature = "sqlite",
-        feature = "odbc",
-        not(feature = "mysql")
-    ))]
-    Postgres, Mssql, Sqlite, Odbc
-}
+    // 2 databases (10 combinations)
+    #[cfg(all(feature = "postgres", feature = "mysql", not(any(feature = "mssql", feature = "sqlite", feature = "odbc"))))]
+    [Postgres, MySql],
+    #[cfg(all(feature = "postgres", feature = "mssql", not(any(feature = "mysql", feature = "sqlite", feature = "odbc"))))]
+    [Postgres, Mssql],
+    #[cfg(all(feature = "postgres", feature = "sqlite", not(any(feature = "mysql", feature = "mssql", feature = "odbc"))))]
+    [Postgres, Sqlite],
+    #[cfg(all(feature = "postgres", feature = "odbc", not(any(feature = "mysql", feature = "mssql", feature = "sqlite"))))]
+    [Postgres, Odbc],
+    #[cfg(all(feature = "mysql", feature = "mssql", not(any(feature = "postgres", feature = "sqlite", feature = "odbc"))))]
+    [MySql, Mssql],
+    #[cfg(all(feature = "mysql", feature = "sqlite", not(any(feature = "postgres", feature = "mssql", feature = "odbc"))))]
+    [MySql, Sqlite],
+    #[cfg(all(feature = "mysql", feature = "odbc", not(any(feature = "postgres", feature = "mssql", feature = "sqlite"))))]
+    [MySql, Odbc],
+    #[cfg(all(feature = "mssql", feature = "sqlite", not(any(feature = "postgres", feature = "mysql", feature = "odbc"))))]
+    [Mssql, Sqlite],
+    #[cfg(all(feature = "mssql", feature = "odbc", not(any(feature = "postgres", feature = "mysql", feature = "sqlite"))))]
+    [Mssql, Odbc],
+    #[cfg(all(feature = "sqlite", feature = "odbc", not(any(feature = "postgres", feature = "mysql", feature = "mssql"))))]
+    [Sqlite, Odbc],
 
-impl_any_decode_for_db! {
-    #[cfg(all(
-        feature = "postgres",
-        feature = "mysql",
-        feature = "sqlite",
-        feature = "odbc",
-        not(feature = "mssql")
-    ))]
-    Postgres, MySql, Sqlite, Odbc
-}
-
-impl_any_decode_for_db! {
-    #[cfg(all(
-        feature = "postgres",
-        feature = "mysql",
-        feature = "mssql",
-        feature = "odbc",
-        not(feature = "sqlite")
-    ))]
-    Postgres, MySql, Mssql, Odbc
-}
-
-impl_any_decode_for_db! {
-    #[cfg(all(
-        feature = "postgres",
-        feature = "mysql",
-        feature = "mssql",
-        feature = "sqlite",
-        not(feature = "odbc")
-    ))]
-    Postgres, MySql, Mssql, Sqlite
-}
-
-// 3 databases (10 combinations)
-impl_any_decode_for_db! {
-    #[cfg(all(
-        feature = "mysql",
-        feature = "mssql",
-        feature = "sqlite",
-        not(any(feature = "postgres", feature = "odbc"))
-    ))]
-    MySql, Mssql, Sqlite
-}
-
-impl_any_decode_for_db! {
-    #[cfg(all(
-        feature = "mysql",
-        feature = "mssql",
-        feature = "odbc",
-        not(any(feature = "postgres", feature = "sqlite"))
-    ))]
-    MySql, Mssql, Odbc
-}
-
-impl_any_decode_for_db! {
-    #[cfg(all(
-        feature = "mysql",
-        feature = "sqlite",
-        feature = "odbc",
-        not(any(feature = "postgres", feature = "mssql"))
-    ))]
-    MySql, Sqlite, Odbc
-}
-
-impl_any_decode_for_db! {
-    #[cfg(all(
-        feature = "mssql",
-        feature = "sqlite",
-        feature = "odbc",
-        not(any(feature = "postgres", feature = "mysql"))
-    ))]
-    Mssql, Sqlite, Odbc
-}
-
-impl_any_decode_for_db! {
-    #[cfg(all(
-        feature = "postgres",
-        feature = "mssql",
-        feature = "sqlite",
-        not(any(feature = "mysql", feature = "odbc"))
-    ))]
-    Postgres, Mssql, Sqlite
-}
-
-impl_any_decode_for_db! {
-    #[cfg(all(
-        feature = "postgres",
-        feature = "mssql",
-        feature = "odbc",
-        not(any(feature = "mysql", feature = "sqlite"))
-    ))]
-    Postgres, Mssql, Odbc
-}
-
-impl_any_decode_for_db! {
-    #[cfg(all(
-        feature = "postgres",
-        feature = "sqlite",
-        feature = "odbc",
-        not(any(feature = "mysql", feature = "mssql"))
-    ))]
-    Postgres, Sqlite, Odbc
-}
-
-impl_any_decode_for_db! {
-    #[cfg(all(
-        feature = "postgres",
-        feature = "mysql",
-        feature = "sqlite",
-        not(any(feature = "mssql", feature = "odbc"))
-    ))]
-    Postgres, MySql, Sqlite
-}
-
-impl_any_decode_for_db! {
-    #[cfg(all(
-        feature = "postgres",
-        feature = "mysql",
-        feature = "odbc",
-        not(any(feature = "mssql", feature = "sqlite"))
-    ))]
-    Postgres, MySql, Odbc
-}
-
-impl_any_decode_for_db! {
-    #[cfg(all(
-        feature = "postgres",
-        feature = "mysql",
-        feature = "mssql",
-        not(any(feature = "sqlite", feature = "odbc"))
-    ))]
-    Postgres, MySql, Mssql
-}
-
-// 2 databases (10 combinations)
-impl_any_decode_for_db! {
-    #[cfg(all(
-        feature = "postgres",
-        feature = "mysql",
-        not(any(feature = "mssql", feature = "sqlite", feature = "odbc"))
-    ))]
-    Postgres, MySql
-}
-
-impl_any_decode_for_db! {
-    #[cfg(all(
-        feature = "postgres",
-        feature = "mssql",
-        not(any(feature = "mysql", feature = "sqlite", feature = "odbc"))
-    ))]
-    Postgres, Mssql
-}
-
-impl_any_decode_for_db! {
-    #[cfg(all(
-        feature = "postgres",
-        feature = "sqlite",
-        not(any(feature = "mysql", feature = "mssql", feature = "odbc"))
-    ))]
-    Postgres, Sqlite
-}
-
-impl_any_decode_for_db! {
-    #[cfg(all(
-        feature = "postgres",
-        feature = "odbc",
-        not(any(feature = "mysql", feature = "mssql", feature = "sqlite"))
-    ))]
-    Postgres, Odbc
-}
-
-impl_any_decode_for_db! {
-    #[cfg(all(
-        feature = "mysql",
-        feature = "mssql",
-        not(any(feature = "postgres", feature = "sqlite", feature = "odbc"))
-    ))]
-    MySql, Mssql
-}
-
-impl_any_decode_for_db! {
-    #[cfg(all(
-        feature = "mysql",
-        feature = "sqlite",
-        not(any(feature = "postgres", feature = "mssql", feature = "odbc"))
-    ))]
-    MySql, Sqlite
-}
-
-impl_any_decode_for_db! {
-    #[cfg(all(
-        feature = "mysql",
-        feature = "odbc",
-        not(any(feature = "postgres", feature = "mssql", feature = "sqlite"))
-    ))]
-    MySql, Odbc
-}
-
-impl_any_decode_for_db! {
-    #[cfg(all(
-        feature = "mssql",
-        feature = "sqlite",
-        not(any(feature = "postgres", feature = "mysql", feature = "odbc"))
-    ))]
-    Mssql, Sqlite
-}
-
-impl_any_decode_for_db! {
-    #[cfg(all(
-        feature = "mssql",
-        feature = "odbc",
-        not(any(feature = "postgres", feature = "mysql", feature = "sqlite"))
-    ))]
-    Mssql, Odbc
-}
-
-impl_any_decode_for_db! {
-    #[cfg(all(
-        feature = "sqlite",
-        feature = "odbc",
-        not(any(feature = "postgres", feature = "mysql", feature = "mssql"))
-    ))]
-    Sqlite, Odbc
-}
-
-// 1 database (5 combinations)
-impl_any_decode_for_db! {
-    #[cfg(all(
-        feature = "postgres",
-        not(any(feature = "mysql", feature = "mssql", feature = "sqlite", feature = "odbc"))
-    ))]
-    Postgres
-}
-
-impl_any_decode_for_db! {
-    #[cfg(all(
-        feature = "mysql",
-        not(any(feature = "postgres", feature = "mssql", feature = "sqlite", feature = "odbc"))
-    ))]
-    MySql
-}
-
-impl_any_decode_for_db! {
-    #[cfg(all(
-        feature = "mssql",
-        not(any(feature = "postgres", feature = "mysql", feature = "sqlite", feature = "odbc"))
-    ))]
-    Mssql
-}
-
-impl_any_decode_for_db! {
-    #[cfg(all(
-        feature = "sqlite",
-        not(any(feature = "postgres", feature = "mysql", feature = "mssql", feature = "odbc"))
-    ))]
-    Sqlite
-}
-
-impl_any_decode_for_db! {
-    #[cfg(all(
-        feature = "odbc",
-        not(any(feature = "postgres", feature = "mysql", feature = "mssql", feature = "sqlite"))
-    ))]
-    Odbc
+    // 1 database (5 combinations)
+    #[cfg(all(feature = "postgres", not(any(feature = "mysql", feature = "mssql", feature = "sqlite", feature = "odbc"))))]
+    [Postgres],
+    #[cfg(all(feature = "mysql", not(any(feature = "postgres", feature = "mssql", feature = "sqlite", feature = "odbc"))))]
+    [MySql],
+    #[cfg(all(feature = "mssql", not(any(feature = "postgres", feature = "mysql", feature = "sqlite", feature = "odbc"))))]
+    [Mssql],
+    #[cfg(all(feature = "sqlite", not(any(feature = "postgres", feature = "mysql", feature = "mssql", feature = "odbc"))))]
+    [Sqlite],
+    #[cfg(all(feature = "odbc", not(any(feature = "postgres", feature = "mysql", feature = "mssql", feature = "sqlite"))))]
+    [Odbc],
 }
